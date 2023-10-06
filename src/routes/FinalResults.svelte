2904a2869fa03f135887d30446dbd7aaf6a2982b
ggfresard <ggfresard@gmail.com>
2023-10-06 10:55:18 -0300

    s3 fix

--- src/routes/FinalResults.svelte
+++ src/routes/FinalResults.svelte
@@ -1,24 +1,39 @@
 <script lang="ts">
-	import { gameState, players } from '$lib/store'
+	import { io } from '$lib/socket'
+	import { Paths, gameState, path, players } from '$lib/store'
 
 	$: places = Array.from(new Set($gameState.players.map((player) => player.points))).sort()
 </script>
 
-{#if $gameState.status === 'finished'}
-	<div class="flex-1 flex items-center flex-col gap-2 p-4">
-		<div class="text-3xl text-delft_blue-400">Resultado Final</div>
-		<div>
-			{#each places as score, i}
-				{#each $gameState.players.filter((p) => p.points === score) as player}
-					<div class="flex gap-2 items-center">
-						{i + 1}. {player.name}
-						{score}
-						{#if i === 0}
-							🏆
-						{/if}
-					</div>
+{#if $gameState.status === 'finish'}
+	<div class="flex-1 flex items-center flex-col gap-2 p-4 z-50 justify-center">
+		<div
+			class=" bg-sunset-400 shadow-lg p-4 flex-col flex gap-2 rounded-lg border-burnt_sienna-400 border-dotted border-2"
+		>
+			<div class="text-3xl text-delft_blue-400 font-bold">Resultado Final</div>
+			<div>
+				{#each places as score, i}
+					{#each $gameState.players.filter((p) => p.points === score) as player}
+						<div class="flex gap-2 items-center justify-between text-delft_blue-400 text-xl">
+							{i + 1}. {player.name}
+							<div class="text-cambridge_blue-200">
+								{#if i === 0}
+									🏆
+								{/if}
+								{score}
+							</div>
+						</div>
+					{/each}
 				{/each}
-			{/each}
+			</div>
+			<button
+				class="w-full"
+				on:click={() => {
+					$path = Paths.None
+					io.disconnect()
+					io.connect()
+				}}>Lobby</button
+			>
 		</div>
 	</div>
 {/if}
